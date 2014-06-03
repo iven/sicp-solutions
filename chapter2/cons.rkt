948ee3b375c0b37848873614255c6490f7415d3c
iven <ivenvd@gmail.com>
2014-06-04 04:08:31 +0800

    Add pair

---
+++ chapter2/cons.rkt
@@ -0,0 +1,21 @@
+#lang racket
+
+(define (cons x y)
+    (define (dispatch m)
+      (cond	((= m 0) x)
+            ((= m 1) y)
+            (else (error "Arguments not 0 or 1 -- CONS" m))))
+    dispatch)
+
+(define (car x) (x 0))
+(define (cdr x) (x 1))
+
+(define (print-cons x)
+    (newline)
+    (display "(")
+    (display (car x))
+    (display ".")
+    (display (cdr x))
+    (display ")"))
+
+(print-cons (cons 10 100))
