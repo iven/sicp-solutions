07a336bb16112df6941a9ba2d2446d3ae26f6991
iven <ivenvd@gmail.com>
2014-06-05 23:56:10 +0800

    Add tree-map

--- chapter2/tree.rkt
+++ chapter2/tree.rkt
@@ -28,6 +28,30 @@
             (else (iter (car items) (iter (cdr items) result)))))
     (iter l null))
 
+(define (scale-tree-origin l factor)
+    (cond ((null? l) null)
+          ((pair? l) (cons (scale-tree-origin (car l) factor)
+                           (scale-tree-origin (cdr l) factor)))
+          (else (* l factor))))
+
+(define (scale-tree l factor)
+    (map
+      (lambda (x)
+        (if (pair? x)
+          (scale-tree x factor)
+          (* x factor)))
+      l))
+
+(define (tree-map f l)
+    (map
+      (lambda (x)
+        (if (pair? x)
+          (tree-map f x)
+          (f x)))
+      l))
+
+(define (square x) (* x x))
+
 (define test1 (cons (list 1 2) (list 3 4)))
 (define test2 (list test1 test1))
 (count-leaves test1)
@@ -35,3 +59,6 @@
 (deep-reverse test1)
 (fringe test1)
 (fringe test2)
+(scale-tree-origin test2 2)
+(scale-tree test2 2)
+(tree-map square test2)
