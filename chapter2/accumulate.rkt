ff3921a3262580b9386f53b7bc98e780580c5601
Xu Lijian <xulijian@qiyi.com>
2014-06-06 18:00:17 +0800

    Add fold-left

--- chapter2/accumulate.rkt
+++ chapter2/accumulate.rkt
@@ -17,14 +17,23 @@
               (map car sequence))
             (accumulate-n op initial (map cdr sequence)))))
 
+(define (fold-right op initial sequence)
+    (accumulate op initial sequence))
+
+(define (fold-left op initial sequence)
+    (define (iter seq result)
+      (if (null? seq)
+        result
+        (iter (cdr seq)
+              (op result (car seq)))))
+    (iter sequence initial))
+
 (define squares (list 0 1 4 9 16 25))
 (define test1 (list (list 1 2 3)
                     (list 4 5 6)
                     (list 7 8 9)
                     (list 10 11 12)))
 
-(accumulate
-  (lambda (x y) (+ x y))
-  0
-  squares)
+(fold-right (lambda (x y) (+ x y)) 0 squares)
+(fold-left / 1 (list 1 2 3))
 (accumulate-n + 0 test1)
