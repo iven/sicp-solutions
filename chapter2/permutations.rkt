0af3b4b3128f23e8fe24696180f089958f94fd7c
iven <ivenvd@gmail.com>
2014-06-06 23:34:58 +0800

    Add permutations

---
+++ chapter2/permutations.rkt
@@ -0,0 +1,15 @@
+#lang racket
+
+(require "accumulate.rkt")
+
+(define (permutations s)
+    (if (null? s)
+      (list null)
+      (flat-map (lambda (x)
+                  (map (lambda (y) (cons x y))
+                       (permutations (remove x s))))
+                s)))
+
+(define test1 (list 1 2 3))
+
+(permutations test1)
