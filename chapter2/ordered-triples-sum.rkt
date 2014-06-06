62bad7f4f33e30351c2f541b62ae93210f66875e
iven <ivenvd@gmail.com>
2014-06-07 00:27:58 +0800

    Add ordered-triples-sum

---
+++ chapter2/ordered-triples-sum.rkt
@@ -0,0 +1,27 @@
+#lang racket
+
+(require "accumulate.rkt")
+(require "enumerate.rkt")
+
+(define (sum-equals? l s)
+    (= s
+       (+ (car l)
+          (cadr l)
+          (cadr (cdr l)))))
+
+(define (unique-pairs n)
+    (flat-map (lambda (x)
+                (flat-map (lambda (y)
+                            (map (lambda (z) (list x y z))
+                                 (enumerate-interval 1 (- y 1))))
+                          (enumerate-interval 1 (- x 1))))
+              (enumerate-interval 1 n)))
+
+(define (ordered-triples-sum n s)
+    (filter (lambda (l) (sum-equals? l s))
+            (unique-pairs n)))
+
+(define test1 (list 1 2 3))
+(sum-equals? test1 6)
+(sum-equals? test1 5)
+(ordered-triples-sum 9 9)
